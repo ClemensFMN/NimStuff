c7373838e76d1ffee062fbb179c9172ffde5bf72
Clemens Novak <clemens@familie-novak.net>
2017-08-17 17:05:32 +0200

    better json example

--- json_1.nim
+++ json_1.nim
@@ -1,23 +1,44 @@
 import json
 
+# parse json
+let jsonNode = parseJson("""
+   {
+     "person": {
+       "name": "Nimmer",
+       "age": 21
+     },
+     "list": [1, 2, 3, 4]
+   }
+""")
 
 type
-  Message* = object
-    username*: string
-    message*: string
+  Person = object
+    name: string
+    age: int
+  
+  Data = object
+    person: Person
+    list: seq[int]
+
+  TestD = object
+    name: string
+    lst: seq[int]
+
+# and unmarshal the parsed json into an object
+var data = to(jsonNode, Data)
+echo data.person.name
+echo data.person.age
+echo data.list
+
+# marshaling an object into json
+echo %*(data)
 
 
+var p1 = Person(name:"Clemens", age:45)
+echo %*(p1)
+# var o1 = Data(person:p1, list:@[1,2,5,6])
+# this does NOT work???
+# echo %*(o1) 
 
-var user = "Clemens"
-var msg = "Hello world"
-
-
-var json_s1 = $(%{"username": %user, "message": %msg})
-
-echo json_s1
-
-var prsd = parseJson(json_s1)
-
-echo prsd
-echo prsd["username"]
-echo prsd["message"]
+var t1 = TestD(name:"kdfjdlkfj", lst:@[1,2,5,6])
+echo %*(t1)
