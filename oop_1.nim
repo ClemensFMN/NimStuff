22774e8d825e4b7c54bac88563f4d8185cefd249
Clemens Novak <clemens@familie-novak.net>
2017-08-17 16:44:56 +0200

    enhanced (clarified?) oop

--- oop_1.nim
+++ oop_1.nim
@@ -5,3 +5,20 @@ type
 
 
 var p = Person(name:"Clemens", age:35)
+echo p
+echo p.name
+
+
+type
+  MyObj = ref object
+    name: string
+    age:int
+
+
+proc `$`(x:MyObj):string =
+  "MyObj:" & x.name
+
+var mo = MyObj(name:"Susi", age:100)
+# this requires the proc `$` for printing
+echo mo
+echo mo.name
